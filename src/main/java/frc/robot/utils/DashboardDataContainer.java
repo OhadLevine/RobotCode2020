9f5f661927ae4d9989acbf93d2c85f0f6befb1c3
Hillel <Amiel>
2020-02-13 21:44:27 +0200

    Update DashboardDataContainer.java

--- src/main/java/frc/robot/utils/DashboardDataContainer.java
+++ src/main/java/frc/robot/utils/DashboardDataContainer.java
@@ -16,30 +16,6 @@ import static frc.robot.Robot.intakeOpener;
 public class DashboardDataContainer {
     
     public DashboardDataContainer() {
-/*       // Mixer dashboard data
-        putNumber("Mixer/Mixer power", 0);
-        putData("Mixer/Spin mixer",
-            new SpinMixer(() -> getNumber("Mixer/Mixer power", 0)));
-        putData("Mixer/Override", new OverrideCommand(mixer,
-            () -> getNumber("Mixer/Mixer power", 0)));
-        // Drivetrain dashboard data
-        putData("Drivetrain/Tune drivetrain rotate PID", new RotateDrivetrain());
-        putData("Drivetrain/Reset Encoders", new RunWhenDisabledCommand(drivetrain::resetEncoders, drivetrain));
-        putData("Drivetrain/Reset Gyro", new RunWhenDisabledCommand(drivetrain::resetGyro, drivetrain));
-        putData("Drivetrain/Calibrate Gyro", new RunWhenDisabledCommand(drivetrain::calibrateGyro, drivetrain));
-        putData("Drivetrain/Reset Odometry", new RunWhenDisabledCommand(drivetrain::resetOdometry, drivetrain));
-        // Shooter dashboard data
-        putNumber("Shooter/Shooting velocity setpoint", ShooterVelocity.kDefault.getVelocity());
-        putData("Shooter/Set cheesy shooting velocity", new CheesySetShooterVelocity(() -> getNumber("Shooter/Shooting velocity setpoint", 0)));
-        putData("Shooter/Set shooting velocity", new SetShooterVelocity(() -> getNumber("Shooter/Shooting Velocity Setpoint", 0)));
-        putData("Shooter/Enable tuning", new StartEndCommand(shooter::enableTuning, shooter::disableTuning));
-        putNumber("Shooter/Override Power", 0);
-        putData("Shooter/Override", new OverrideCommand(shooter,
-            () -> getNumber("Shooter/Override Power", 0)));
-        // Loader dashboard data
-        putNumber("Loader/Loader Power", 0);
-        putData("Loader/Override", new OverrideCommand(loader,
-            () -> getNumber("Loader/Loader Power", 0)));*/
         // Intake dashboard data
         putNumber("Intake/Intake power", 0);
         putData("Intake/Override intake", new OverrideCommand(intake,
@@ -52,10 +28,6 @@ public class DashboardDataContainer {
             () -> intakeOpener.getDefaultCommand().stopTuning(), intakeOpener));
         putData("IntakeOpener/Open", new SetDesiredOpenerAngle(true));
         putData("IntakeOpener/Close", new SetDesiredOpenerAngle(false));
-        // Command groups data
-/*        putData("CommandGroup/Auto Shoot", new AutoShoot());
-        putData("CommandGroup/Collect Cell", new CollectCell());
-        putData("CommandGroup/Collect From Feeder", new CollectFromFeeder());*/
     }
 
     public void update() {
