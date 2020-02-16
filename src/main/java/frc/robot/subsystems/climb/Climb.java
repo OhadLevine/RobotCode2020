af59e4cd746e5eaaa2aba233e5680f41c1f793c8
Hillel <Amiel>
2020-02-16 07:40:23 +0200

    start of work on the Climb with AnalogPotentiometer

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -7,6 +7,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.EncoderType;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.oblarg.oblog.Loggable;
 
@@ -15,6 +18,7 @@ import static frc.robot.Robot.robotConstants;
 public class Climb extends SubsystemBase implements Loggable {
     private WPI_TalonSRX hookTalonSRX;
     private CANSparkMax climbSparkMax;
+    private AnalogPotentiometer hookPotentiometer;
 
     /**
      * The climb holds all the methods used for the robots climb in the endgame.
@@ -23,6 +27,7 @@ public class Climb extends SubsystemBase implements Loggable {
      */
     public Climb() {
         hookTalonSRX = new WPI_TalonSRX(robotConstants.can.kHookTalonSRX);
+        hookPotentiometer = new AnalogPotentiometer(int channel, double fullRange, double offset);
         climbSparkMax = new CANSparkMax(robotConstants.can.kClimbSparkMax, MotorType.kBrushless);
 
         hookTalonSRX.configSupplyCurrentLimit(
