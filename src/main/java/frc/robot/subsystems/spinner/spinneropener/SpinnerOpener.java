1fed87483b559a0870456e581ee991a0fa35eeea
Ohad <ohadlevin99@gmail.com>
2020-03-03 23:10:45 +0200

    Create SpinnerOpener.java

---
+++ src/main/java/frc/robot/subsystems/spinner/spinneropener/SpinnerOpener.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.spinner.spinneropener;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SpinnerOpener extends SubsystemBase {
+  /**
+   * This class holds all of the methods for the subsystem that open the spinner.
+   */
+  public SpinnerOpener() {
+
+  }
+}
